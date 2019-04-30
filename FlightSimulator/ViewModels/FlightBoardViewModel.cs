77f840330d23d0308c8918a54be711179a6e2286
liran <liranusa6@gmail.com>
2019-04-30 17:54:15 +0300

    added classes

--- FlightSimulator/ViewModels/FlightBoardViewModel.cs
+++ FlightSimulator/ViewModels/FlightBoardViewModel.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlightSimulator.Views;
+using System.Windows.Input;
+using FlightSimulator.Model;
 
 namespace FlightSimulator.ViewModels
 {
