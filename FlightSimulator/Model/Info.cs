6ad9a7aad5bd991bcba896db66b8213033fc888e
liran <liranusa6@gmail.com>
2019-04-30 18:48:32 +0300

    icon

--- FlightSimulator/Model/Info.cs
+++ FlightSimulator/Model/Info.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using FlightSimulator.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FlightSimulator.Model
 {
-    class Info
+    /**
+     * class info incharge of the connection of the simlator to the lightgear and the info transfermation between them.
+     */
+   public class Info
     {
+
     }
 }
